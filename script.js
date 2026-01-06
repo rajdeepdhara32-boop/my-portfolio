79e43ff84a64d7099a391d418a4b5791c230fa64
Rajdeep <rajdeepdhara32@gmail.com>
2026-01-07 03:50:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  reveals.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    if (top < window.innerHeight - 150) {
+      section.classList.add('active');
+    }
+  });
+});
